aaec1a8dc1165744365cd3c37ec36859856942ff
wmc2tokyo <wmcooper2@yahoo.com>
2019-01-27 16:13:52 +0900

    Removed data files.

--- gamedata/customprepositions.py
+++
@@ -1,8 +0,0 @@
-prepositions = [
-    "under the bed",
-    "in the park",
-    "in the sky",
-    "by my mother",
-    "in the room",
-    
-]
--- gamedata/extraforeignwords.py
+++
@@ -1,6 +0,0 @@
-"""This module is for holding foreign words that are used in the English
-    language but are not a part of the Total English book series.
-
-    Returns None.
-"""
-foreign_words = ['piano','dynamo','kilo','kimono','photo','soprano']
